33198cb13f27447155bb45f0039447c12da439fd
likunling <likunling@kjtpay.com.cn>
2017-02-08 18:26:18 +0800

    增加实体

---
+++ yunpan/yunpan-service/src/main/java/www/yunpan/com/entity/RoleEntity.java
@@ -0,0 +1,20 @@
+package www.yunpan.com.entity;
+//http://blog.csdn.net/driverking/article/details/6769553
+public class RoleEntity {
+	private Integer roleId;
+	private String roleName;
+	public Integer getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(Integer roleId) {
+		this.roleId = roleId;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	
+
+}
---
+++ yunpan/yunpan-service/src/main/java/www/yunpan/com/entity/UserEntity.java
@@ -0,0 +1,41 @@
+package www.yunpan.com.entity;
+
+public class UserEntity {
+	private Integer userId;
+	private String loginname;
+	private String username;
+	private String password;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getLoginname() {
+		return loginname;
+	}
+
+	public void setLoginname(String loginname) {
+		this.loginname = loginname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
